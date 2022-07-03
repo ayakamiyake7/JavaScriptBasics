6cd0664875f78332270aaf875f90cdcf742f7191
Ayaka Miyake <ayakamiyake7@gmail.com>
2022-07-03 07:04:32 +0100

    Object.

--- js/main.js
+++ js/main.js
@@ -143,9 +143,16 @@
   //   console.log(r); //4
   //   console.log(others); //color:"red",y:180
 
-  const point = { x: 100, y: 180 };
-  const keys = Object.keys(point);
-  keys.forEach((key) => {
-    console.log(`Key: ${key}`);
-  });
+  //   const point = { x: 100, y: 180 };
+  //   const keys = Object.keys(point);
+  //   keys.forEach((key) => {
+  //     console.log(`Key: ${key} Value: ${point[key]}`);
+  //   });
+
+  const points = [
+    { x: 30, y: 20 },
+    { x: 10, y: 50 },
+    { x: 40, y: 40 },
+  ];
+  console.log(points[1]["y"]);
 }
